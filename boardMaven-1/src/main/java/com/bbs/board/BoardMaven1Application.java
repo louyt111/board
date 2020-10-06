aa4d7ca44affcbbbfbfb532e81e0beef43491bc5
koje <koje@B202-20>
2020-10-06 11:27:25 +0900

    init release

---
+++ boardMaven-1/src/main/java/com/bbs/board/BoardMaven1Application.java
@@ -0,0 +1,13 @@
+package com.bbs.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardMaven1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardMaven1Application.class, args);
+	}
+
+}
